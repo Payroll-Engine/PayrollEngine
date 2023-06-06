2fd5fb7d33a17b1a36dbeb27c14c92e09b6beac1
Jani Giannoudis <jani@giannoudis.ch>
2023-06-06 07:45:21 +0200

    updated minimal payroll example and image time calc

--- Examples/MinimalPayroll/Setup.cmd
+++ Examples/MinimalPayroll/Setup.cmd
@@ -2,5 +2,5 @@
 call PayrollConsole TenantDelete MinimalPayroll /trydelete
 call PayrollConsole PayrollImport Payroll.json
 call PayrollConsole PayrollImport Payroll.Values.json
-rem call PayrollConsole PayrollImport Payroll.Jobs.json
-rem call PayrollConsole PayrollResults MinimalPayroll 2 /wait
+call PayrollConsole PayrollImport Payroll.Jobs.json
+call PayrollConsole PayrollResults MinimalPayroll 10 /wait
