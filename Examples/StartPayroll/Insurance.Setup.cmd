781fca42a319c3c3bbb9ed55c37a2a3bdb5187b8
Jani Giannoudis <jani@giannoudis.ch>
2023-06-20 13:46:05 +0200

    updated start payroll

--- Examples/StartPayroll/Insurance.Setup.cmd
+++ Examples/StartPayroll/Insurance.Setup.cmd
@@ -2,5 +2,5 @@
 call PayrollConsole TenantDelete StartTenant /trydelete
 call PayrollConsole PayrollImport Basic.json
 call PayrollConsole PayrollImport Insurance.json
-call PayrollConsole PayrunEmployeeTest Insurance.Test.et.json
+call PayrollConsole PayrunEmployeeTest Insurance.Test.et.json /showall
 pause
