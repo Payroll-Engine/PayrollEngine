a810e2a00620167026637e1f877f4d11b4cdd8bb
Jani Giannoudis <jani@giannoudis.ch>
2026-03-09 07:37:11 +0100

    ci: add release workflow

--- Examples/SimplePayroll/scripts/GetCurrentAge.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using PayrollEngine.Client.Scripting.Function;
-
-public static class AgeExtension
-{
-    public static int GetCurrentAge(this Function function, DateTime? dateTime) => 
-        dateTime.HasValue ? System.DateTime.UtcNow.Year - dateTime.Value.Year : 0;
-}    
--- Examples/SimplePayroll/scripts/GetDiseaseDaysCount.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using PayrollEngine.Client.Scripting;
-using PayrollEngine.Client.Scripting.Function;
-
-public static class SickExtensions
-{
-    public static decimal GetDiseaseDaysCount(this WageTypeValueFunction function) => 
-        function.CaseValue["Disease"].TotalDaysByValue();
-}    
